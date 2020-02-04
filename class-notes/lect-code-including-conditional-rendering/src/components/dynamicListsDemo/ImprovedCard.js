501cbda303f32fc7ef167f4772f8cf6357359eb6
zgrnk <jakelyons87@gmail.com>
2020-02-04 11:54:40 +0000

    update ImprovedCard to be a class based component, instead of a functional comp

--- class-notes/lect-code-including-conditional-rendering/src/components/dynamicListsDemo/ImprovedCard.js
+++ class-notes/lect-code-including-conditional-rendering/src/components/dynamicListsDemo/ImprovedCard.js
@@ -3,9 +3,9 @@ import React from 'react';
 const oscarsEl = <p>Got the Oscar Award! 😉 </p>;
 const noOscarsEl = <p>Great movie but no Oscars! 😔 </p>;
 
-// NOTE this function does exactly the same thing as the ternary operator on line 35
-// it also does the same thing as uncommenting both lines 41 & 42, for the conditional rendering example
-// uncomment line 38 to use this function
+// NOTE this function does exactly the same thing as the ternary operator on line 36
+// it also does the same thing as uncommenting both lines 42 & 43, for the conditional rendering example
+// uncomment line 39 to use this function
 const displayOscarsText = (hasOscars) => {
   if (hasOscars) {
     return oscarsEl;
@@ -14,38 +14,41 @@ const displayOscarsText = (hasOscars) => {
   }
 }
 
-const ImprovedCard = (props) => {
-  // NOTE awardText cannot use a ternary operator or conditional rendering because it has three possible outcomes
-  let awardText;
-  if (props.hasOscars) {
-    if (props.IMDbRating >= 9) {
-      awardText = <p> WOW! Oscar Award and IMDb rating {props.IMDbRating}! </p>;
-    } else if (props.IMDbRating >= 7) {
-      awardText = <p> Got the Oscar Award and has IMDb rating {props.IMDbRating}, not bad at all! </p>;
+class ImprovedCard extends React.Component {
+  render() {
+    // NOTE awardText cannot use a ternary operator or conditional rendering because it has three possible outcomes
+    let awardText;
+    if (this.props.hasOscars) {
+      if (this.props.IMDbRating >= 9) {
+        awardText = <p> WOW! Oscar Award and IMDb rating {this.props.IMDbRating}! </p>;
+      } else if (this.props.IMDbRating >= 7) {
+        awardText = <p> Got the Oscar Award and has IMDb rating {this.props.IMDbRating}, not bad at all! </p>;
+      }
+    } else {
+      awardText = <p> Great movie but no Oscars! Has IMDb rating {this.props.IMDbRating}. </p>;
     }
-  } else {
-    awardText = <p> Great movie but no Oscars! Has IMDb rating {props.IMDbRating}. </p>;
+    return (
+      <div  className="movies-list-item">
+        <h2>{this.props.title}</h2>
+        <p>Director: {this.props.director}</p>
+
+        {/* NOTE below shows a ternary operator. Personally I would use it for this example instead of conditional rendering*/}
+        {/* {this.props.hasOscars ? oscarsEl : noOscarsEl} */}
+
+        {/* NOTE below shows an example of a function todo the exact same thing as above, and also same as the conditional rendering below */}
+        {/* {displayOscarsText(this.props.hasOscars)} */}
+
+        {/* NOTE the two lines below are examples of conditional rendering. these next two lines accomplish the same thing as line 36*/}
+        {/* { this.props.hasOscars && oscarsEl }
+        { !this.props.hasOscars && noOscarsEl } */}
+
+        {awardText}
+
+        <button onClick={this.props.clickToDelete}>Delete!</button>
+      </div>
+    );
   }
-  return (
-    <div  className="movies-list-item">
-      <h2>{props.title}</h2>
-      <p>Director: {props.director}</p>
 
-      {/* NOTE below shows a ternary operator. Personally I would use it for this example instead of conditional rendering*/}
-      {/* {props.hasOscars ? oscarsEl : noOscarsEl} */}
-
-      {/* NOTE below shows an example of a function todo the exact same thing as above, and also same as the conditional rendering below */}
-      {/* {displayOscarsText(props.hasOscars)} */}
-
-      {/* NOTE the two lines below are examples of conditional rendering. these next two lines accomplish the same thing as line 35*/}
-      {/* { props.hasOscars && oscarsEl }
-      { !props.hasOscars && noOscarsEl } */}
-
-      {awardText}
-
-      <button onClick={props.clickToDelete}>Delete!</button>
-    </div>
-  );
 };
 
 export default ImprovedCard;
